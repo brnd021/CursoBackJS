df603df4cba36adf1158c7ec9c20a4794bc5b40f
brnd021 <bernardocustodio686@gmail.com>
2025-12-03 18:28:35 -0300

    feat: Add multiple utility functions for string manipulation, number processing, and validation

---
+++ codeWars.js
@@ -0,0 +1,179 @@
+//Count how many characters appear more than twice.”
+
+function duplicateCount(text){
+    const char = text.toLowerCase().split('');
+    let contagem = []
+    char.forEach((c) => {
+        if (char.indexOf(c) !== char.lastIndexOf(c)){
+            if(!contagem.includes(c) ){
+                contagem.push(c)
+            } 
+        }
+    });
+    return contagem.length;
+}
+
+// Return the next square if sq is a perfect square, -1 otherwise
+
+
+function findNextSquare(sq) {
+    const square = Math.sqrt(sq)
+    const squareChar = String(square).split('')
+    let point = false
+    squareChar.forEach(e=>{
+        if(e === '.') point = true
+    })
+    if(point) return -1
+    else return Math.pow((square+1),2)
+}
+
+//Take 2 strings s1 and s2 including only letters from a to z. 
+//Return a new sorted string (alphabetical ascending), the longest possible, 
+//containing distinct letters - each taken only once - coming from s1 or s2.
+
+function longest(s1,s2){
+    const array1 = [...s1].sort();
+    const array2 = [...s2].sort();
+    const newArray = [];
+    for(const char of array1.concat(array2)){
+        if(!newArray.includes(char)){
+            newArray.push(char);
+        }
+    }
+
+    return newArray.sort().join('')
+}
+
+//Square every digit of a number and concatenate them.
+
+function squareDigits(num){
+    const array = [...String(num)]
+    for(let i = 0; i < array.length;i++){
+        array[i] = Math.pow(array[i],2)
+    }
+    const number = array.join('')
+    return Number(number)
+}
+
+//Given an array of integers, find the one that appears an odd number of times.
+//There will always be only one integer that appears an odd number of times.
+
+function findOdd(a){
+    let length = a.length
+    let array = a.sort()
+    let atual = array[0]-1
+    for(let i = 0;i<length;i++){
+        if(array[i] !== atual){
+            atual = array[i]
+            if((array.lastIndexOf(array[i])-i)%2 === 0) return atual
+        }
+    }
+}
+
+//Implement the function unique_in_order which takes as argument a sequence and 
+//returns a list of items without any elements with the same value next to each 
+//other and preserving the original order of elements.
+
+function uniqueInOrder(a){
+    let array = [...a]
+    let newArray = []
+    for(let i = 0; i<array.length;i++)
+        if(array[i]!=array[i-1]) newArray.push(array[i])
+    return newArray
+}
+
+//Implement a function that adds two numbers together and returns their sum in binary. 
+//The conversion can be done before, or after the addition.
+//The binary number returned should be a string.
+
+function addBinary(a,b){
+    let sum = a + b
+    if(sum === 0 ) return 0
+    let array = []
+    while(sum > 0){
+        array.push(sum % 2)
+        sum = Math.floor(sum/2); 
+    }
+    return array.reverse().join('')
+}
+
+
+//In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G".
+//Your function receives one side of the DNA (string, except for Haskell); 
+//you need to return the other complementary side.
+//DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+function dnaStrand(dna){
+    let arrayDna = [...dna]
+    arrayDna.forEach((e,i)=>{
+        if(e === "A") arrayDna[i] = "T"
+            else if( e === "T") arrayDna[i] = "A"
+                else if( e === "C") arrayDna[i] = "G"
+                else arrayDna[i] = "C"
+    })
+    return arrayDna.join("")
+}
+
+//Write a function that accepts an array of 10 integers (between 0 and 9), 
+//that returns a string of those numbers in the form of a phone number.
+
+function createPhoneNumber(numbers){
+    let phoneNumber = ["(",,,,")"," ",,,,"-",,,,]
+    let pos = 0
+    for(let i = 1; i < 14; i++){
+        if(i != 4 && i != 5 && i != 9){
+            phoneNumber[i]=numbers[pos];
+            pos++
+        }
+    }
+    return phoneNumber.join("")
+}
+
+//Write a function that takes an integer as input, and returns the number of bits that are equal to one
+//in the binary representation of that number. You can guarantee that input is non-negative.
+
+function countBits(n) {
+    if( n < 0 ) return 0;
+    let count = 0
+     while (n > 0) {
+        if (n % 2 === 1) count++;  
+        n = Math.floor(n/2);                  
+    }
+    return count
+}
+
+//Write a function that takes a string of braces, and determines if the order of the braces is valid. 
+//It should return true if the string is valid, and false if it's invalid.
+//This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}.
+// All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+// What is considered Valid?
+// A string of braces is considered valid if all braces are matched with the correct brace.
+
+function validBraces(braces){
+  let array = [...braces].sort()
+  while(true){
+    if(array[0] === "("){
+        if(array[1] !== "(" && array[1] !== ")") return false
+        if((array.lastIndexOf(")")+2)%2 === 0) return false
+        array.splice(array.lastIndexOf(")"),1)
+        array.splice(0,1)
+    } 
+    else if(array[0] === "["){
+        if(array[1] !== "[" && array[1] !== "]") return false
+        if((array.lastIndexOf("]")+2)%2 === 0) return false
+        array.splice(array.lastIndexOf("}"),1)
+        array.splice(0,1)
+    } 
+    else if(array[0] === "{"){
+        if(array[1] !== "{" && array[1] !== "}") return "false1"
+        if((array.lastIndexOf("}")+2)%2 === 0) return "false3"
+        array.splice(array.lastIndexOf("}"),1)
+        array.splice(0,1)
+    }
+    else break; 
+  }
+  if(array.length>1) return array
+  else return true
+}
+
+console.log(validBraces("(){}[]"))
