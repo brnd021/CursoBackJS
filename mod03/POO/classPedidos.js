73d48063b2683b39aaed61082f4ebb2116671a84
brnd021 <bernardocustodio686@gmail.com>
2026-01-23 16:12:39 -0300

    feat: Add database management files including SQL exercises and database structure

--- mod03/POO/classPedidos.js
+++ mod03/POO/classPedidos.js
@@ -2,4 +2,17 @@ class Pedidos{
     constructor(lista){
         this.pedidos = lista
     }
+    calcularTotal(){
+
+    }
+    finalizarPedido(){
+
+    }
+}
+
+class PedidoVazioError extends Error{
+    constructor(mensagem){
+        super(mensagem);
+        this.name = this.constructor.name;
+    }
 }
